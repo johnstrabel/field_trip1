0400d6902c8ba19db2dc949436620640fe0fc981
JohnStrabel <johnny.strabes@gmail.com>
2025-06-18 01:28:16 +0200

    📱 Complete Info.plist with location permissions and fixed app name

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
-import Flutter
 import UIKit
+import Flutter
+import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,9 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // Add your Google Maps API key here
+    GMSServices.provideAPIKey("AIzaSyD5JeOHX4RSsS81pG98SPCTF1cfBL8_jts")
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
